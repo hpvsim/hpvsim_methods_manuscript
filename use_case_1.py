bf8d993e57973453f726328c1baf8cdec0371890
Robyn Stuart <robyna.s@gmail.com>
2022-12-09 12:38:39 +1100

    shorter runtimes

--- use_case_1.py
+++ use_case_1.py
@@ -95,16 +95,17 @@ def make_sim(setting=None):
 
     # Parameters
     pars = dict(
-        n_agents        = [10e3,5e3][debug],
+        n_agents        = [50e3,5e3][debug],
         dt              = [0.5,1.0][debug],
-        start           = [1975,1980][debug],
+        start           = [1975,2000][debug],
         end             = 2060,
-        burnin          = 25,
+        burnin          = [25,0][debug],
         network         = 'default',
         condoms         = dict(m=0, c=0, o=0),
         debut           = debut[setting],
         mixing          = mixing[setting],
-        use_multiscale  = True,
+        use_multiscale  = False,
+        genotypes       = [16,18,'hrhpv'],
     )
 
     sim = hpv.Sim(pars)
@@ -163,10 +164,10 @@ def run_scens(setting, verbose=0):
     #%% Run as a script
 if __name__ == '__main__':
 
-    # sim = make_sim(setting='s1')
-    # sim.run(verbose=0.1)
+    sim = make_sim(setting='s1')
+    sim.run(verbose=0.1)
 
-    for setting in ['s1', 's2', 's3']:
-        scen = run_scens(setting, verbose=0.1)
+    # for setting in ['s1', 's2', 's3']:
+    #     scen = run_scens(setting, verbose=0.1)
 
     print('Done.')
