c31483c0c3ae3d0ac4892400daf69c45c0a8ee0e
Jamie Cohen <jamie.alex.cohen@gmail.com>
2022-12-19 21:19:15 -0800

    STOPPING HERE!

--- calib_tool.py
+++ calib_tool.py
@@ -148,16 +148,28 @@ def run_calib_tool(calib_pars=None):
     sim.initialize()
     # Create sim to get baseline prognoses parameters
     if calib_pars is not None:
-        print(calib_pars)
-        calib_pars['genotype_pars'].hpv18['dur_dysp']['par2'] = 1
-        calib_pars['genotype_pars'].hpv16['dur_dysp']['par2'] = 6
+
+        ## hpv 16 pars
+        calib_pars['genotype_pars'].hpv16['dur_dysp']['par2'] = 3.8 #4
+        calib_pars['genotype_pars'].hpv16['dur_dysp']['par1'] = 7.25 #13
+        calib_pars['genotype_pars'].hpv16['prog_rate'] = 0.17 #0.099
+        calib_pars['genotype_pars'].hpv16['cancer_prob'] = 0.022 # 0.017
+
+        ## hpv 18 pars
+        calib_pars['genotype_pars'].hpv18['dur_dysp']['par2'] = 0.75
+        calib_pars['genotype_pars'].hpv18['rel_beta'] = 1.22
+        calib_pars['genotype_pars'].hpv18['cancer_prob'] = 0.13
+        # calib_pars['genotype_pars'].hpv18['prog_rate'] = 0.9
+
+        ## hr hpv pars
         calib_pars['genotype_pars'].hrhpv['dur_dysp']['par2'] = 8
-        calib_pars['genotype_pars'].hpv16['dur_dysp']['par1'] = 13
-        calib_pars['genotype_pars'].hrhpv['dur_dysp']['par1'] = 18
-        calib_pars['genotype_pars'].hpv18['rel_beta'] = 1.25
-        calib_pars['genotype_pars'].hpv18['cancer_prob'] = 0.15
-        calib_pars['genotype_pars'].hpv18['prog_rate'] = 0.9
-        calib_pars['genotype_pars'].hrhpv['prog_rate'] = 0.08
+        # calib_pars['genotype_pars'].hrhpv['dur_dysp']['par1'] = 18
+        calib_pars['genotype_pars'].hrhpv['rel_beta'] = 0.75
+        calib_pars['genotype_pars'].hrhpv['cancer_prob'] = 0.0026
+        # calib_pars['genotype_pars'].hrhpv['prog_rate'] = 0.08
+
+        print(calib_pars)
+
         sim.update_pars(calib_pars)
 
     sim.run()
