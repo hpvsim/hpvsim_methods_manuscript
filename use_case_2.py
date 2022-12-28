1fe9eeec4a3003edf08a568f53890f19c680feb3
Jamie Cohen <jamie.alex.cohen@gmail.com>
2022-12-28 04:28:09 -0800

    test

--- use_case_2.py
+++ use_case_2.py
@@ -48,8 +48,8 @@ to_run = [
     # 'run_sims',
     # 'plot_mixing',
     # 'plot_sims',
-    # 'run_scenarios',
-    'plot_scenarios',
+    'run_scenarios',
+    # 'plot_scenarios',
 ]
 
 #%% Define parameters
