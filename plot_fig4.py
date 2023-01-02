ebdd0cd7e9a7427ad981224283efddef3f5a9078
Robyn Stuart <robyna.s@gmail.com>
2023-01-02 13:45:19 +1100

    add script to make table

--- plot_fig4.py
+++ plot_fig4.py
@@ -14,6 +14,7 @@ import seaborn as sns
 
 #%% Plotting function
 def plot_fig4(calib_pars=None):
+
     # Group genotypes
     genotypes = ['hpv16', 'hpv18', 'hrhpv']
     sim = hpv.Sim(genotypes=genotypes)
